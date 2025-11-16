573e29b8f5a9a07ba4e14fef65bc0db040ea5036
Ahmad-030 <ahmadasif20222@gmail.com>
2025-11-16 15:43:43 +0500

    SplashADDED

--- proj.js
+++ proj.js
@@ -612,4 +612,14 @@ if (viewMoreBtn) {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
         });
+
+    
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
 // --- END NEW PROJECT TOGGLE LOGIC ---
